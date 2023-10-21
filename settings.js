114c0416e4d8052cd6ffab55f2d52780f324db32
Beatrice Baret <bbaret@techer.digital>
2023-10-22 00:28:49 +0200

    firstSettings

---
+++ settings.js
@@ -0,0 +1,25 @@
+const settingsButton = document.querySelector(".settings");
+const settingsContainer = document.querySelector("#settingsContainer");
+
+let isOpen = false;
+
+settingsButton.addEventListener("click", () => {
+
+    isOpen = !isOpen
+
+    if (isOpen) {
+        settingsContainer.classList.remove("settingsContainer");
+        settingsContainer.classList.add("settingsContainerOpen");
+        settingsButton.style.color = "#000";
+    } else {
+        settingsContainer.classList.add("settingsContainer");
+        settingsContainer.classList.remove("settingsContainerOpen");
+        setTimeout(() => {
+            settingsButton.style.color = "";
+        }, 200);
+
+    }
+
+    console.log("ok");
+})
+
